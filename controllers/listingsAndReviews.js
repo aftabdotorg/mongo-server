3db1780163d5fe176603964ee6055cd3ebdbdb51
Aftab Akhand <aft4b.akhand@gmail.com>
2023-08-12 12:12:53 +0530

    redis caching 1

--- controllers/listingsAndReviews.js
+++ controllers/listingsAndReviews.js
@@ -1,4 +1,5 @@
 import ListingsAndReview from "../models/listingsAndReviews.js";
+import { client } from "../index.js";
 
 const getListingsAndReviwes = async (req, res) => {
   const { page } = req.query;
@@ -7,15 +8,31 @@ const getListingsAndReviwes = async (req, res) => {
   const totalCount = await ListingsAndReview.count();
   if (page) {
     if (page * 10 < totalCount) hasNextPage = true;
-
+    if (await client.get(`listingAndReviews?page=${page}`)) {
+      console.log("Cache hit");
+      return res.send({
+        data: JSON.parse(await client.get(`listingAndReviews?page=${page}`)),
+        totalCount,
+        count: JSON.parse(await client.get(`listingAndReviews?page=${page}`))
+          .length,
+        hasNextPage,
+      });
+    }
+    console.log("Cache miss");
     data = await ListingsAndReview.find()
       .skip((page - 1) * 10)
       .limit(10);
+    await client.setEx(
+      `listingAndReviews?page=${page}`,
+      15,
+      JSON.stringify(data)
+    );
 
     return res.send({ data, totalCount, count: data.length, hasNextPage });
   }
   data = await ListingsAndReview.find().limit(10);
 
+  await client.setEx(`listingAndReviews`, 15, JSON.stringify(data));
   return res.send({ data, totalCount, hasNextPage, count: 10 });
 };
 const createListingsAndReviwes = async (req, res) => {
--- index.js
+++ index.js
@@ -3,8 +3,15 @@ import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./db.js";
 connectDB();
+
 import listingRoutes from "./routes/listings.js";
 import listingAndReviewsRoutes from "./routes/listingsAndReviews.js";
+
+import { createClient } from "redis";
+const client = createClient();
+client.on("error", (err) => console.log(`Redis Client Error: ${err}`));
+await client.connect();
+export { client };
 
 const app = express();
 app.use(express.json()); // ! userfull for parsing // bodyParser
