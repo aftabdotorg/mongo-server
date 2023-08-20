c857183270e51b532342f83d62e8c861a6be3b48
Aftab Akhand <aft4b.akhand@gmail.com>
2023-08-21 02:14:32 +0530

    redis cloud setup

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ import express from "express";
 import dotenv from "dotenv";
 import { createClient } from "redis";
 
-import rateLimit from 'express-rate-limit'
+import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
-	// windowMs: 15 * 60 * 1000, // 15 minutes
-	windowMs: 10000,
-	max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-	// store: ... , // Use an external store for more precise rate limiting
-})
-
+  // windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 10000,
+  max: 5, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  // store: ... , // Use an external store for more precise rate limiting
+});
 
 dotenv.config();
 import connectDB from "./db.js";
@@ -21,7 +20,14 @@ connectDB();
 import listingRoutes from "./routes/listings.js";
 import listingAndReviewsRoutes from "./routes/listingsAndReviews.js";
 
-const client = createClient();
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_CONNECTION_STRING,
+    port: 10770,
+  },
+});
+
 client.on("error", (err) => console.log(`Redis Client Error: ${err}`));
 await client.connect();
 export { client };
@@ -29,7 +35,7 @@ export { client };
 const app = express();
 app.use(express.json()); // ! userfull for parsing // bodyParser
 // Apply the rate limiting middleware to all requests
-app.use(limiter)
+app.use(limiter);
 const PORT = 5007;
 
 // ! everything begining with /listing is present in function
